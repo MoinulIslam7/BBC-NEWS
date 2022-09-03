95328e1d48afd5e3ce41b027cb133e982affd477
MoinulIslam7 <moinul180180@gmail.com>
2022-09-04 01:33:00 +0600

    spinner correction

--- js/NewsDetails.js
+++ js/NewsDetails.js
@@ -1,4 +1,5 @@
 const loadCategoryDetails = (category_id) => {
+    
     const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
     fetch(url)
         .then(res => res.json())
@@ -6,6 +7,7 @@ const loadCategoryDetails = (category_id) => {
         .catch((error) => {
             console.log(error)
         });
+        toggleSpinner(true);
 }
 
 const toggleSpinner = (isLoading) => {
@@ -18,66 +20,67 @@ const toggleSpinner = (isLoading) => {
     }
 }
 const displayCategoryDetails = (news) => {
+
+    
+    news.sort(function (a, b) { return b.total_view - a.total_view })
+    processData(news);
+
+}
+
+const processData = (news) => {
+
     const categoryDetails = document.getElementById('category-details');
     categoryDetails.textContent = '';
-
-
     const countItem = document.getElementById('count-item')
     countItem.innerText = '';
     let count = 0;
-    toggleSpinner(true);
-
-
     news.forEach(category => {
-        
         const categoryDetailsDiv = document.createElement('div');
         categoryDetailsDiv.classList.add('card');
         categoryDetailsDiv.classList.add('mb-4');
+
+
         categoryDetailsDiv.innerHTML = `
-    <div class="row">
-    <div class="col-md-4">
-                    <img src="${category.thumbnail_url}" class="img-fluid rounded-start" alt="...">
-                  </div>
-                  <div class="col-md-8">
-                    <div class="card-body mb-2">
-                      <h5 class="card-title">${category.title}</h5>
-                      <div>
-                      <p class="card-text text" >${category.details}</p>
-                      </div>
-                    </div>
-                    <div class="d-flex  justify-content-evenly">
-                    <img src="${category.author.img}" class="img-fluid rounded-circle image" alt="...">
-                        <div>
-                        <h6>Name: ${category.author.name ? category.author.name : "No Name found"}</h6>
-                        <p>${category.author.published_date}</p>
-                        </div>
-                        <div>
-                            <p>${category.total_view ? category.total_view : "No Views Found"}</p>
-                        </div>
-                        <div>
-                            <p>${category.rating.number}</p>
-                        </div>
-                        <div>
-                            <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newsDetailModal "onclick="loadEveryNewsDetails('${category._id}')" >Details</button>
-                        </div>
-                      </div>
-                    </div>
-    </div>
-                    `;
-         
+        <div class="row">
+        <div class="col-md-4">
+        <img src="${category.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-8">
+        <div class="card-body mb-2">
+        <h5 class="card-title">${category.title}</h5>
+        <div>
+        <p class="card-text text" >${category.details}</p>
+        </div>
+        </div>
+        <div class="d-flex  justify-content-evenly">
+        <img src="${category.author.img}" class="img-fluid rounded-circle image" alt="...">
+        <div>
+        <h6>Name: ${category.author.name ? category.author.name : "No Name found"}</h6>
+        <p>${category.author.published_date}</p>
+        </div>
+        <div>
+        <p>${category.total_view ? category.total_view : "No Views Found"}</p>
+        </div>
+        <div>
+        <p>${category.rating.number}</p>
+        </div>
+        <div>
+        <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newsDetailModal "onclick="loadEveryNewsDetails('${category._id}')" >Details</button>
+        </div>
+        </div>
+        </div>
+        </div>`;
+
 
         categoryDetails.appendChild(categoryDetailsDiv);
         count++;
     })
     toggleSpinner(false);
-   
     if (count === 0) {
         countItem.innerText = `No News Found`;
     }
     else {
         countItem.innerText = `${count} News found`
     }
-
     
-
 }
--- js/category.js
+++ js/category.js
@@ -11,7 +11,6 @@ const displayCategory = (categoryList) => {
     const categoryField = document.getElementById('category-list');
 
     // display all category
-    let i=0;
     categoryList.forEach(category => {
         
         
@@ -19,9 +18,8 @@ const displayCategory = (categoryList) => {
         categoryDiv.classList.add('nav-item');
        
         categoryDiv.innerHTML = `
-                                <a onclick="loadCategoryDetails('${category.category_id}')" id="item-${i}" class="nav-link active mx-2" aria-current="page" href="#">${category.category_name}</a>
+                                <a onclick="loadCategoryDetails('${category.category_id}')" class="nav-link active mx-2" aria-current="page" href="#">${category.category_name}</a>
                     `;
-        i = i+1;
         categoryField.appendChild(categoryDiv);
         
     })
